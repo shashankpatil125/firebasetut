f8afd63d98ffc8d31fa42c7f9005cf24a8e47454
shashank lalit patil <shashankpatil125@gmail.com>
2022-04-09 09:05:43 +0000

    connected firebase

--- fb/src/Component/Form.js
+++ fb/src/Component/Form.js
@@ -10,45 +10,79 @@ function Form() {
         message: ""
     });
 
-    let name,value;
+    let name, value;
     function dalinfo(event) {
-        name=event.target.name;
-        value=event.target.value;
+        name = event.target.name;
+        value = event.target.value;
         console.log(value);
 
-        setinfo({ ... info, [name]:[value] })
+        setinfo({ ...info, [name]: [value] })
     }
 
-    
-    
+
+    const postd = async (e) => {
+        e.preventDefault();
+
+        const{name,email,mobile,address,message}=info;  //object destructring
+
+        const res = await fetch("https://fir-tut-27b89-default-rtdb.firebaseio.com/firebasetut.json",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name:name,
+                    email,
+                    mobile,
+                    address,
+                    message
+                })
+            }
+
+        )
+        if(res){
+            setinfo({
+                name: "",
+        email: "",
+        mobile: "",
+        address: "",
+        message: ""
+            })
+        }
+        console.log("clicked")
+    }
 
 
     return (
         <div>
-            <h1>CONTACT US</h1>
+            <form method='POST'>
 
-           
-            <div>
-                <p>Enter your name</p>
-                <input type='text' name='name' onChange={dalinfo} value={info.name} ></input>
-            </div>
-            <div>
-                <p>Email</p>
-                <input type='text' name="email" onChange={dalinfo} value={info.email}></input>
-            </div>
-            <div>
-                <p>Mobile no</p>
-                <input type='text' name="mobile" onChange={dalinfo} value={info.mobile}></input>
-            </div>
-            <div>
-                <p>Address</p>
-                <input type='text' name='address' onChange={dalinfo} value={info.address}></input>
-            </div>
-            <div>
-                <p>Mwssage</p>
-                <input type='text' name='message' onChange={dalinfo} value={info.message}></input>
-            </div>
-            <button>SUBMIT</button>
+                <h1>CONTACT US</h1>
+
+
+                <div>
+                    <p>Enter your name</p>
+                    <input type='text' name='name' onChange={dalinfo} value={info.name} required></input>
+                </div>
+                <div>
+                    <p>Email</p>
+                    <input type='text' name="email" onChange={dalinfo} value={info.email} required></input>
+                </div>
+                <div>
+                    <p>Mobile no</p>
+                    <input type='text' name="mobile" onChange={dalinfo} value={info.mobile} required></input>
+                </div>
+                <div>
+                    <p>Address</p>
+                    <input type='text' name='address' onChange={dalinfo} value={info.address} required></input>
+                </div>
+                <div>
+                    <p>Mwssage</p>
+                    <input type='text' name='message' onChange={dalinfo} value={info.message} required></input>
+                </div>
+                <button onClick={postd} >SUBMIT</button>
+            </form>
         </div>
     )
 }
