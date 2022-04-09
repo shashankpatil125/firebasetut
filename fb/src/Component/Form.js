51a7aa98e163441d37f49b0663a00927d299c1b5
shashank lalit patil <shashankpatil125@gmail.com>
2022-04-09 07:05:09 +0000

    event

--- fb/src/Component/Form.js
+++ fb/src/Component/Form.js
@@ -1,49 +1,54 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 function Form() {
 
-    const [info, setinfo]=useState({
-        name:"",
-        email:"",
-        mobile:"",
-        address:"",
-        message:""
+    const [info, setinfo] = useState({
+        name: "",
+        email: "",
+        mobile: "",
+        address: "",
+        message: ""
     });
 
-    
-    let name,email,mobile,address,message;
-    function dalinfo(event){
-        name=name.target.name
-        console.log(event.target.name);
-        
+    let name,value;
+    function dalinfo(event) {
+        name=event.target.name;
+        value=event.target.value;
+        console.log(value);
+
+        setinfo({ ... info, [name]:[value] })
     }
+
+    
+    
 
 
     return (
         <div>
             <h1>CONTACT US</h1>
-            
-                <div>
-                    <p>Enter your name</p>
-                    <input type='text' onChange={dalinfo} ></input>
-                </div>
-                <div>
-                    <p>Email</p>
-                    <input type='text' onChange={dalinfo}></input>
-                </div>
-                <div>
-                    <p>Mobile no</p>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <p>Address</p>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <p>Mwssage</p>
-                    <input type='text'></input>
-                </div>
-                <button>SUBMIT</button>
+
+           
+            <div>
+                <p>Enter your name</p>
+                <input type='text' name='name' onChange={dalinfo} value={info.name} ></input>
+            </div>
+            <div>
+                <p>Email</p>
+                <input type='text' name="email" onChange={dalinfo} value={info.email}></input>
+            </div>
+            <div>
+                <p>Mobile no</p>
+                <input type='text' name="mobile" onChange={dalinfo} value={info.mobile}></input>
+            </div>
+            <div>
+                <p>Address</p>
+                <input type='text' name='address' onChange={dalinfo} value={info.address}></input>
+            </div>
+            <div>
+                <p>Mwssage</p>
+                <input type='text' name='message' onChange={dalinfo} value={info.message}></input>
+            </div>
+            <button>SUBMIT</button>
         </div>
     )
 }
